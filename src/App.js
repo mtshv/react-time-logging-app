0a4293ce8fe5b882011e8248e6ee48ba75d534c0
mtshv <i.mityushev@gmail.com>
2018-05-18 21:40:28 +0300

    Start/Stop functionality was added to timer

--- src/App.js
+++ src/App.js
@@ -37,6 +37,14 @@ class App extends Component {
         this.deleteTimer(timerId);
     };
 
+    handleStartClick = (timerId) => {
+        this.startTimer(timerId);
+    };
+
+    handleStopClick = (timerId) => {
+        this.stopTimer(timerId);
+    };
+
     createTimer = (timer) => {
         const t = newTimer(timer);
         this.setState({
@@ -65,6 +73,40 @@ class App extends Component {
         });
     };
 
+    startTimer = (timerId) => {
+        const now = Date.now();
+
+        this.setState({
+            timers: this.state.timers.map((timer) => {
+                if (timer.id === timerId) {
+                    return Object.assign({}, timer, {
+                        runningSince: now,
+                    });
+                } else {
+                    return timer;
+                }
+            }),
+        });
+    };
+
+    stopTimer = (timerId) => {
+        const now = Date.now();
+
+        this.setState({
+            timers: this.state.timers.map((timer) => {
+                if (timer.id === timerId) {
+                    const lastElapsed = now - timer.runningSince;
+                    return Object.assign({}, timer, {
+                        elapsed: timer.elapsed + lastElapsed,
+                        runningSince: null,
+                    });
+                } else {
+                    return timer;
+                }
+            }),
+        });
+    };
+
     render() {
         return (
             <div className='ui three column centered grid'>
@@ -73,6 +115,8 @@ class App extends Component {
                         timers={this.state.timers}
                         onFormSubmit={this.handleEditFormSubmit}
                         onTrashClick={this.handleTrashClick}
+                        onStartClick={this.handleStartClick}
+                        onStopClick={this.handleStopClick}
                     />
                     <AddNewTimer
                         onFormSubmit={this.handleCreateFormSubmit}
--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {renderElapsedString} from "../utils";
+import TimerActionButton from "./TimerActionButton";
 
 class Timer extends Component {
     componentDidMount() {
@@ -9,6 +10,14 @@ class Timer extends Component {
     componentWillUnmount() {
         clearInterval(this.forceUpdateInterval);
     }
+
+    handleStartClick = () => {
+        this.props.onStartClick(this.props.id);
+    };
+
+    handleStopClick = () => {
+        this.props.onStopClick(this.props.id);
+    };
 
     handleTrashClick = () => {
         this.props.onTrashClick(this.props.id);
@@ -38,9 +47,11 @@ class Timer extends Component {
                         </span>
                     </div>
                 </div>
-                <div className='ui bottom attached blue basic button'>
-                    Start
-                </div>
+                <TimerActionButton
+                    timerIsRunning={!!this.props.runningSince}
+                    onStartClick={this.handleStartClick}
+                    onStopClick={this.handleStopClick}
+                />
             </div>
         );
     }
