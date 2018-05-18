cbfa44786110645fcb2b9c4a12ab95d0a17b4020
mtshv <i.mityushev@gmail.com>
2018-05-18 20:39:13 +0300

    State was added to app.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 import AddNewTimer from "./Components/AddNewTimer";
 import EditableTimerList from "./Components/EditableTimerList";
+import uuid from 'uuid4';
 
 class App extends Component {
+    state = {
+        timers: [
+            {
+                title: 'Practice squat',
+                project: 'Gym Chores',
+                id: uuid(),
+                elapsed: 5456099,
+                runningSince: Date.now(),
+            },
+            {
+                title: 'Bake squash',
+                project: 'Kitchen Chores',
+                id: uuid(),
+                elapsed: 1273998,
+                runningSince: null,
+            },
+        ],
+    };
+
     render() {
         return (
             <div className='ui three column centered grid'>
